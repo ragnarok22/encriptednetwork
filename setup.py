6e3acf191e801fdbc2bc880181013e01eecda4a4
Reinier Hernández Ávila <rhernandeza@facinf.uho.edu.cu>
2016-06-02 15:13:56 -0400

    añadiendo al setup la configuracion para compilar el .exe

--- setup.py
+++ setup.py
@@ -1,14 +1,40 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+import platform
+from glob import glob
+from main import __version__, __appname__, __author__, __author_email__
 
-setup(
-    name='Encripted Network',
-    version='1.3',
-    description='This application using Shannon-Fano algorithm to encoding messages given the'
-                ' probabilities for each text',
-    author='Reinier Hernández Ávila',
-    author_email='rhernandeza@facinf.uho.edu.cu',
-    # url='http://www.encriptednetwork.com',
-    license='GPL',
-    scripts=['main.py'],
-    packages=find_packages(),
-)
+if platform == 'win32':
+    import py2exe
+
+SETUP_DICT = {
+    'windows': [{
+        'script': 'main.py',
+        'icon_resources': [(0, r'images\\logo.ico')]
+    }],
+
+    'name': __appname__,
+    'version': __version__,
+    'description': 'This application using Shannon-Fano algorithm to encoding messages',
+    'author': __author__,
+    'author_email': __author_email__,
+
+    'zipfile': None,
+    # 'zipfile': 'lib/library.zip',
+
+    'data_files': (
+        ('', glob(r'C:\Windows\SYSTEM32\msvcp100.dll')),
+        ('', glob(r'C:\Windows\SYSTEM32\msvcr100.dll')),
+        ('images', ['images\logo.png']),
+        ('images', ['images\shannon.png']),
+        ('', ['styles.css']),
+    ),
+
+    'options': {
+        'py2exe': {
+            'bundle_files': 0,
+            'includes': ['sip', 'PyQt4.QtCore'],
+        },
+    }
+}
+
+setup(**SETUP_DICT)
